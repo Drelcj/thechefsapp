bc83effe16cfb286651ec62f4c3c71fecf3d5941
Emmanuel Chijioke <manuelckay@gmail.com>
2024-01-17 11:00:05 +0100

    Initial commit to set up landing page

---
+++ src/app/school/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const school = () => {
+  return (
+    <div>school landing page</div>
+  )
+}
+
+export default school
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const shop = () => {
+  return (
+    <div>shop landing page</div>
+  )
+}
+
+export default shop
